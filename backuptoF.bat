d0f7659ea9e9a6d869d369e3af5e84d3e3698625
Your Name <you@example.com>
2025-08-21 19:46:17 +0100

    'reer'

--- backuptoF.bat
+++ backuptoF.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:: Database settings
 set DATABASE_NAME=orghdata
 set USER=postgres
 set BACKUP_DIR=C:\backups
@@ -8,22 +9,26 @@ set EXTERNAL_SSD_DRIVE=D:
 set EXTERNAL2=F:
 set EXTERNAL3=G:
 
+:: Get timestamp (YYYYMMDD_HHMMSS)
+for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set ldt=%%I
+set TIMESTAMP=!ldt:~0,8!_!ldt:~8,6!
 
-
-set TIMESTAMP=!DAY!!MONTH!!YEAR!
-
-set BACKUP_FILE=%BACKUP_DIR%\backup.sql
+:: Backup file with timestamp
+set BACKUP_FILE=%BACKUP_DIR%\backup_!TIMESTAMP!.sql
 
 echo Backing up database %DATABASE_NAME%...
 
-REM Set PGPASSFILE environment variable to point to .pgpass file
+:: Set PGPASSFILE environment variable to point to .pgpass file
 set PGPASSFILE=C:\Users\Public\pgpass.conf
 
 "C:\bin\pg_dump.exe" -h localhost -U %USER% -b -v -f "%BACKUP_FILE%" %DATABASE_NAME%
+
 echo Backup completed.
 
-echo Copying backup to external SSD drive %EXTERNAL_SSD_DRIVE%...
+echo Copying backup to external SSD drives...
 copy "%BACKUP_FILE%" %EXTERNAL_SSD_DRIVE%
 copy "%BACKUP_FILE%" %EXTERNAL2%
+copy "%BACKUP_FILE%" %EXTERNAL3%
 echo Copy completed.
+
 endlocal
