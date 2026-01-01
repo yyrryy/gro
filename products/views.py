5ba5f0a796652f487c987fa568cb002a344a1f99
Your Name <you@example.com>
2026-01-01 10:58:01 +0100

    'reer'

--- products/views.py
+++ products/views.py
@@ -8641,7 +8641,7 @@ def yeardatabl(request):
 def yeardatabachat(request):
     this_year=datetime.now().year
     year=request.GET.get('year') or this_year
-    print('year>>>', year)
+    print('year>>>', year) 
     # get all bls of that year
     bons=Itemsbysupplier.objects.filter(date__year=year).order_by('-id')[:50]
     trs=''
