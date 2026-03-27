cb308bc26226243f62550175ca8ed2e32708d2e3
Your Name <you@example.com>
2026-03-27 12:04:26 +0100

    'reer'

--- waitress.py
+++ waitress.py
@@ -1,22 +1,80 @@
-import os
+# import os
+# from threading import Thread
+# from time import sleep
+# import subprocess
+# import sys
+
+# # os.system('start /B waitress-serve 192.168.1.8 --port=80 Gro.wsgi:application')
+# def runserver():
+#     os.system('waitress-serve --host=192.168.1.8 --port=80 Gro.wsgi:application')
+
+# def lunchchrome():
+#     # ensure the django server is up and running
+#     sleep(2)
+#     # get ipv4 address
+#     os.system('start chrome http://192.168.1.8')
+# t1=Thread(target=runserver)
+
+# t2=Thread(target=lunchchrome)
+
+# t1.start()
+# sleep(2)
+# t2.start()
+
+import subprocess
+import socket
 from threading import Thread
 from time import sleep
-import subprocess
-import sys
 
-# os.system('start /B waitress-serve 192.168.1.8 --port=80 Gro.wsgi:application')
+
+HOST = "192.168.1.8"
+PORT = 80
+
+
 def runserver():
-    os.system('waitress-serve --host=192.168.1.8 --port=80 Gro.wsgi:application')
+    """
+    Start Waitress server with optimized settings
+    """
+    subprocess.Popen([
+        "waitress-serve",
+        f"--host={HOST}",
+        f"--port={PORT}",
+        "--threads=8",
+        "--connection-limit=200",
+        "--channel-timeout=30",
+        "Gro.wsgi:application"
+    ])
 
-def lunchchrome():
-    # ensure the django server is up and running
-    sleep(2)
-    # get ipv4 address
-    os.system('start chrome http://192.168.1.8')
-t1=Thread(target=runserver)
 
-t2=Thread(target=lunchchrome)
+def wait_for_server(host, port):
+    """
+    Wait until server is actually accepting connections
+    """
+    while True:
+        try:
+            with socket.create_connection((host, port), timeout=1):
+                break
+        except OSError:
+            sleep(1)
 
-t1.start()
-sleep(2)
-t2.start()
+
+def launch_chrome():
+    """
+    Open browser only after server is ready
+    """
+    wait_for_server(HOST, PORT)
+    subprocess.Popen([
+        "cmd", "/c", "start", "chrome", f"http://{HOST}"
+    ])
+
+
+if __name__ == "__main__":
+    t1 = Thread(target=runserver)
+    t2 = Thread(target=launch_chrome)
+
+    t1.start()
+    sleep(1)  # small buffer
+    t2.start()
+
+    t1.join()
+    t2.join()
